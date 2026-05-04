e751eb5cc792248d50d2119fe7c7d58059615302
abirham <abirhammuch526@gmail.com>
2026-05-04 18:05:09 +0300

    contact email

--- abirham-portfolio/src/components/Contact.jsx
+++ abirham-portfolio/src/components/Contact.jsx
@@ -3,6 +3,7 @@ import { Mail, MapPin, Phone, Send } from 'lucide-react';
 import { FaTelegramPlane } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
+import { MdEmail} from "react-icons/md";
 
 import emailjs from '@emailjs/browser';
 
@@ -120,6 +121,9 @@ const Contact = () => {
                 </a>
                 <a href="https://et.linkedin.com/in/abirham-muche-4aa7a6320" target="_blank" rel="noopener noreferrer" className="p-3 rounded-xl bg-slate-100 dark:bg-slate-700 text-slate-700 dark:text-slate-300 hover:bg-slate-200 dark:hover:bg-slate-600 transition-all duration-300 hover:scale-110">
                   <FaLinkedin className="w-5 h-5" />
+                </a>
+                <a href="https://et.linkedin.com/in/abirham-muche-4aa7a6320" target="_blank" rel="noopener noreferrer" className="p-3 rounded-xl bg-slate-100 dark:bg-slate-700 text-slate-700 dark:text-slate-300 hover:bg-slate-200 dark:hover:bg-slate-600 transition-all duration-300 hover:scale-110">
+                  <MdEmail className="w-5 h-5" />
                 </a>
               </div>
             </div>
